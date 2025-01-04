8173b69ef022975e34a5a210c2cf9f4383ba80cf
Bill Tubbs <bill.tubbs@me.com>
2025-01-04 12:55:45 -0800

    Refactor and added docstrings.

--- sysid_demo.py
+++ sysid_demo.py
@@ -1,20 +1,22 @@
+"""This example is adapted from Joel Andersson, Joris Gillis, Moritz Diehl, 
+KU Leuven, 2010-2023. See CasADi documentation.
+
+In this example, we fit a nonlinear model to measurements
+
+Since the number of control intervals is potentially very large here,
+we use memory-efficient Map and MapAccum, in combination with
+code generation.
+
+We will be working with a 2-norm objective:
+|| y_measured - y_simulated ||_2^2
+
+This form is well-suited for the Gauss-Newton Hessian approximation.
+"""
+
 import numpy as np
 import casadi as cas
 from casadi import DM, MX, Function
 from sysid_utils import (make_n_step_simulator, make_gauss_newton_solver)
-
-
-# In this example, we fit a nonlinear model to measurements
-#
-# This example uses more advanced constructs than the vdp* examples:
-# Since the number of control intervals is potentially very large here, 
-# we use memory-efficient Map and MapAccum, in combination with
-# code generation.
-#
-# We will be working with a 2-norm objective:
-# || y_measured - y_simulated ||_2^2
-#
-# This form is well-suited for the Gauss-Newton Hessian approximation.
 
 
 ############ SETTINGS #####################
@@ -50,21 +52,27 @@ ode = Function('ode', [states, controls, params], [rhs])
 ############ Creating a simulator ##########
 n_steps_per_sample = 10
 dt = 1 / fs / n_steps_per_sample
-one_sample = make_n_step_simulator(ode, dt, states, controls, params, n_steps=n_steps_per_sample)
+sample_step = make_n_step_simulator(ode, dt, states, controls, params, n_steps=n_steps_per_sample)
+print(sample_step)
+
+# f.mapaccum works on functions with the signature:
+#  f: (x, u) -> (x_next , y )
+all_samples = sample_step.mapaccum("all_samples", N)
 
 
 ############ Simulating the system ##########
-print("\nSimulating the system")
-
-all_samples = one_sample.mapaccum("all_samples", N)
 
 # Choose an excitation signal
 np.random.seed(0)
 u_data = DM(0.1 * np.random.random(N))
 
+# Initial condition
 x0 = DM([0, 0])
+
 X_measured = all_samples(x0, u_data, cas.repmat(param_truth, 1, N))
 
+# Output equation
+#   y(t) = x1(t)
 y_data = X_measured[0, :].T
 
 # You may add some noise here
@@ -73,38 +81,34 @@ y_data = X_measured[0, :].T
 
 
 ############ Identifying the simulated system: single shooting strategy ##########
-print("\nIdentifying the simulated system: single shooting strategy")
 
 # Note, it is in general a good idea to scale your decision variables such
 # that they are in the order of ~0.1..100
-X_symbolic = all_samples(x0, u_data, cas.repmat(params*scale, 1, N))
+X_symbolic = all_samples(x0, u_data, cas.repmat(params * scale, 1, N))
 
-e = y_data - X_symbolic[0, :].T
-nlp = {'x': params, 'f': 0.5 * cas.dot(e, e)}
+errors = y_data - X_symbolic[0, :].T
 
-solver = make_gauss_newton_solver(e, nlp, params)
+solver = make_gauss_newton_solver(errors, params)
 sol = solver(x0=param_guess)
 
 print(f"Solution: {sol['x'] * scale}")
 
-assert(cas.norm_inf(sol["x"] * scale-param_truth) < 1e-8)
+assert cas.norm_inf(sol["x"] * scale-param_truth) < 1e-8
 
 
 ############ Identifying the simulated system: multiple shooting strategy ##########
-print("\nIdentifying the simulated system: multiple shooting strategy")
 
 # All states become decision variables
 X = MX.sym("X", 2, N)
 
-Xn = one_sample.map(N, 'openmp')(X, u_data.T, cas.repmat(params*scale, 1, N))
+Xn = sample_step.map(N, 'openmp')(X, u_data.T, cas.repmat(params * scale, 1, N))
 
 gaps = Xn[:, :-1] - X[:, 1:]
 
-e = y_data-Xn[0, :].T
+errors = y_data - Xn[0, :].T
 
 V = cas.veccat(params, X)
-
-nlp = {'x': V, 'f': 0.5 * cas.dot(e, e), 'g': cas.vec(gaps)}
+g = cas.vec(gaps)
 
 # Multiple shooting allows for careful initialization
 yd = np.diff(y_data, axis=0) * fs
@@ -112,9 +116,9 @@ X_guess = cas.horzcat(y_data, cas.vertcat(yd, yd[-1])).T
 
 x0 = cas.veccat(param_guess, X_guess)
 
-solver = make_gauss_newton_solver(e, nlp, V)
+solver = make_gauss_newton_solver(errors, V, g)
 sol = solver(x0=x0, lbg=0, ubg=0)
 
 print(f"Solution: {sol['x'][:4] * scale}")
 
-assert(cas.norm_inf(sol["x"][:4] * scale-param_truth) < 1e-8)
+assert cas.norm_inf(sol["x"][:4] * scale-param_truth) < 1e-8
--- sysid_utils.py
+++ sysid_utils.py
@@ -1,7 +1,6 @@
 import warnings
-import numpy as np
 import casadi as cas
-from casadi import DM, MX, Function
+from casadi import MX, Function
 
 
 # Use just-in-time compilation if available
@@ -20,9 +19,28 @@ else:
     COMPILER = ''
 
 
-def make_one_step_simulator(ode, dt, states, controls, params):
+def make_one_step_simulator(ode, dt, states, controls, params, name='RK4'):
     """Create a one-step-ahead simulator function using Runge Kutta 4
     integration scheme.
+
+    Arguments:
+        ode : Function 
+            CasADi function for the righthand side of the ODE equation
+            with the signature: ode(states, controls, params).
+        dt : float
+            Timestep size.
+        states : MX.sym
+            Vector representing the state variables.
+        controls : MX.sym
+            Vector representing the control inputs.
+        params : MX.sym
+            Vector of parameter values.
+        name : str
+            Name to give to returned function.
+
+    Returns:
+        Function(name, [states, controls, params], [states_final])
+
     """
 
     # Runge Kutta 4 integration scheme
@@ -33,37 +51,93 @@ def make_one_step_simulator(ode, dt, states, controls, params):
     states_final = states + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
     # Create CasADi function
-    return Function('one_step', [states, controls, params], [states_final])
+    return Function(name, [states, controls, params], [states_final])
 
 
-def make_n_step_simulator(ode, dt, states, controls, params, n_steps=10):
+def make_n_step_simulator(ode, dt, states, controls, params, n_steps=10, name=None):
     """Create a simulator function that simulates an n-step-ahead
     propagation of the system.
+
+    Arguments:
+        ode : Function 
+            CasADi function for the righthand side of the ODE equation
+            with the signature: ode(states, controls, params).
+        dt : float
+            Timestep size.
+        states : MX.sym
+            Vector representing the state variables.
+        controls : MX.sym
+            Vector representing the control inputs.
+        params : MX.sym
+            Vector of parameter values.
+        n_steps : int
+            Number of consequtive timesteps over which to repeat the 
+            integration.
+        name : str
+            Name to give to returned function.  If name is None, a
+            name will be automatically generated based on the name of the 
+            ode function and n_steps.
+    
+    Returns:
+        Function(name, [states, controls, params], [X])
+
     """
-    one_step = make_one_step_simulator(ode, dt, states, controls, params)
+    step = make_one_step_simulator(ode, dt, states, controls, params)
     X = states
     for _ in range(n_steps):
-        X = one_step(X, controls, params)
+        X = step(X, controls, params)
+
+    if name is None:
+        name = f"{step.name()}_{n_steps}_steps"
 
     # Create CasADi function
-    return Function('n_step', [states, controls, params], [X])
+    return Function(name, [states, controls, params], [X])
 
 
-def make_gauss_newton_solver(e, nlp, V, with_jit=WITH_JIT, compiler=COMPILER):
-    """Create a Gauss-Newton solver."""
-    J = cas.jacobian(e, V)
+def make_gauss_newton_solver(
+    errors, 
+    x, 
+    g=None, 
+    name="solver", 
+    solver="ipopt", 
+    with_jit=WITH_JIT, 
+    compiler=COMPILER
+):
+    """Create a Gauss-Newton solver to solve an NLP of the form:
+
+        minimize f(x)
+        subject to:
+            x_lb ≤ x ≤ x_ub
+            g_lb ≤ g(x) ≤ g_ub
+
+    using sequential quadratic programming (SQP) approach.
+
+    Starting from a given initial guess for the primal and dual variables 
+    (x(0), λ(0)), it solves the NLP by iteratively computing local convex 
+    quadratic approximations at the current iterate (x(k), λ(k)) and 
+    solving them using a quadratic programming (QP) solver.
+
+    See the following:
+      - https://www.syscop.de/files/2015ss/events/tempo/e3_gn.pdf
+
+    """
+
+    J = cas.jacobian(errors, x)
     H = cas.triu(cas.mtimes(J.T, J))
     sigma = MX.sym("sigma")
     hess_lag = Function(
-        'nlp_hess_l', 
-        {'x': V, 'lam_f': sigma, 'hess_gamma_x_x': sigma * H},
+        'nlp_hess_l',
+        {'x': x, 'lam_f': sigma, 'hess_gamma_x_x': sigma * H},
         ['x', 'p', 'lam_f', 'lam_g'],
         ['hess_gamma_x_x'],
         {"jit": with_jit, "compiler": compiler}
     )
+    nlp = {'x': x, 'f': 0.5 * cas.dot(errors, errors)}
+    if g is not None:
+        nlp['g'] = g
     return cas.nlpsol(
-        "solver", 
-        "ipopt", 
+        name,
+        solver,
         nlp,
         {"hess_lag": hess_lag, "jit": with_jit, "compiler": compiler}
     )
